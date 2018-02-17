0a3e5286b68a48a940893dc38f4b276a6eb5ddfa
Marcin Rachwalski <rachwalski.marcin@gmail.com>
2018-02-17 07:47:12 +0100

    Initial commit

---
+++ app/src/main/gen/com/example/udacity/surfconnect/Manifest.java
@@ -0,0 +1,28 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.udacity.surfconnect;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+<application
+    android:allowBackup="true"
+    android:icon="@mipmap/ic_launcher"
+    android:label="@string/app_name"
+    android:roundIcon="@mipmap/ic_launcher_round"
+    android:supportsRtl="true"
+    android:theme="@style/AppTheme">
+<meta-data android:name="com.facebook.accountkit.ApplicationName"
+    android:value="@string/app_name" />
+<meta-data android:name="com.facebook.sdk.ApplicationId"
+    android:value="@string/FACEBOOK_APP_ID" />
+<meta-data android:name="com.facebook.accountkit.ClientToken"
+    android:value="@string/ACCOUNT_KIT_CLIENT_TOKEN" />
+
+<activity
+    android:name="com.facebook.accountkit.ui.AccountKitActivity"
+    android:theme="@style/AppLoginTheme"
+    tools:replace="android:theme">
+</activity>
+
+}
+
